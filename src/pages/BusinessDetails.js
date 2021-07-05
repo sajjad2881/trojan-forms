b78da3c633b3fd45745c281c42fd25403f45e9e8
Kevin <kevin.yi93@gmail.com>
2021-07-05 17:52:10 -0400

    Refactor business details page to use react-hook-forms

--- src/pages/BusinessDetails.js
+++ src/pages/BusinessDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import FormHeader from '../components/FormHeader';
 import { useHistory } from "react-router-dom";
 import { Pane,
@@ -7,133 +7,78 @@ import { Pane,
          SelectField,
          SavedIcon,
          CaretRightIcon,
-         Button
+         Button,
+         Text
        } from 'evergreen-ui';
+import { useForm } from "react-hook-form";
+import { industries, staffSizes } from "../utils/data";
 
 const BusinessDetailsForm = () => {
   const history = useHistory();
-  const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    email: '',
-    phone: '',
-    typeOfBusiness: '',
-    businessCategory: ''
-  });
-
-  const onChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  }
+  const { register, handleSubmit } = useForm();
 
   const onClickBack = () => {
     history.push("/");
   }
 
+  const onSubmit = (data) => {
+    console.log("DATA", data)
+  };
+
   return (
     <div>
       <FormHeader title="Business Details (1/5)" onClickBack={onClickBack} />
       <Pane padding="1rem">
-        <Strong size={300}>Tell us about your business</Strong>
+        <Strong>Tell us about your business</Strong>
         <TextInputField
-          name="name"
-          value={formData.name}
-          placeholder="Brand/Shop Name"
-          hint="Name your customer will see"
-          marginTop="1.5rem"
-          width="100%"
+          placeholder="Commercial Name"
           style={{height: "5rem"}}
-          onChange={onChange}
-        />
-        <TextInputField
-          name="description"
-          value={formData.description}
-          placeholder="Business Description"
-          hint="Enter business description here"
-          width="100%"
-          style={{height: "5rem"}}
-          onChange={onChange}
-        />
-        <TextInputField
-          name="email"
-          value={formData.email}
-          placeholder="Email Address"
-          hint="Enter your e-mail address here"
-          marginTop="1.5rem"
-          width="100%"
-          style={{height: "5rem"}}
-          onChange={onChange}
-        />
-        <TextInputField
-          name="phone"
-          value={formData.phone}
-          placeholder="Mobile Number"
-          hint="Enter your phone number here"
-          width="100%"
-          autoComplete="off"
-          style={{height: "5rem"}}
-          onChange={onChange}
+          hint="Name"
+          {...register("name")}
         />
         <SelectField
-          name="typeOfBusiness"
-          value={formData.typeOfBusiness}
-          hint="Type of Business"
-          width="100%"
           style={{height: "5rem"}}
-          onChange={onChange}
           marginBottom="2rem"
+          hint="Industry"
+          {...register("industry")}
         >
-          <option value="individual" selected>
-            Individual
-          </option>
-          <option value="sole proprietor">
-            Sole Proprietor
-          </option>
-          <option value="partnership">
-            Partnership
-          </option>
-          <option value="private limited company">
-            Private Limited Company
-          </option>
-          <option value="public limited company">
-            Public Limited Company
-          </option>
+          {industries.map((industry, idx) => {
+            return (
+              <option value={`${industry.toLowerCase()}`} selected={idx === 0}>
+                {industry}
+              </option>
+            )
+          })}
         </SelectField>
         <SelectField
-          name="businessCategory"
-          value={formData.businessCategory}
-          hint="Business Category"
-          width="100%"
           style={{height: "5rem"}}
-          onChange={onChange}
+          marginBottom="2rem"
+          hint="Staff Size"
+          {...register("staffSize")}
         >
-          <option value="variety store" selected>
-            Variety Store
-          </option>
-          <option value="general store">
-            General Store
-          </option>
-          <option value="bus lines">
-            Bus Lines
-          </option>
-          <option value="taxi cabs/limousines">
-            Tax Cabs/Limousines
-          </option>
-          <option value="motion picture theatre">
-            Motion Picture Theatre
-          </option>
-          <option value="education">
-            Education
-          </option>
-          <option value="all other mcc">
-            All Other MCC
-          </option>
-          <option value="hospitality">
-            Motion Picture Theatre
-          </option>
+          {staffSizes.map((size, idx) => {
+            return (
+              <option value={`${size.toLowerCase()}`} selected={idx === 0}>
+                {size}
+              </option>
+            )
+          })}
         </SelectField>
+        <Strong>What Safepay services will you use?</Strong>
+        <Pane marginBottom="2rem">
+          <Pane display="flex" alignItems="center">
+            <input name="checkout" type="checkbox" {...register('checkout')} id="checkout" />
+            <Text marginLeft="1rem">Checkout</Text>
+          </Pane>
+          <Pane display="flex" alignItems="center">
+            <input name="quicklinks" type="checkbox" {...register('quicklinks')} id="quicklinks" />
+            <Text marginLeft="1rem">Quick Links</Text>
+          </Pane>
+          <Pane display="flex" alignItems="center">
+            <input name="billPayments" type="checkbox" {...register('billPayments')} id="billPayments" />
+            <Text marginLeft="1rem">Bill Payments</Text>
+          </Pane>
+        </Pane>
         <Pane display="flex" justifyContent="space-between">
           <Button
             height="45px"
@@ -142,6 +87,7 @@ const BusinessDetailsForm = () => {
             backgroundColor="#ffa700"
             color="white"
             fontSize={24}
+            onClick={handleSubmit(onSubmit)}
           >
             Save
           </Button>
