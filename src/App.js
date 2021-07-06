62e5a7dba699f40840cac4bcb75aa5f3e547afcc
Kevin <kevin.yi93@gmail.com>
2021-07-06 13:38:23 -0400

    Delete unused component page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Home from './pages/Home';
 import BusinessDetailsForm from './pages/BusinessDetails';
-import PaymentMethod from './pages/PaymentMethod';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/details' component={BusinessDetailsForm} />
-        <Route exact path='/payment' component={PaymentMethod} />
       </Switch>
     </Router>
   );
